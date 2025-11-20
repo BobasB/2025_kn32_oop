a99db586401fc3fb48e871a235ee765e412a8de4
BobasB <bugil.bogdan@gmail.com>
2025-11-20 09:43:51 +0200

    почали робити бібліотеку

--- 4_lab/main.py
+++ 4_lab/main.py
@@ -1,4 +1,5 @@
 import new_file
 
+print(f"{__name__} виконується.")
 if __name__ == "__main__":
-    print("Запуск головної програми.")
+    print("Запуск головної програми в main.py.")
--- 4_lab/new_file.py
+++ 4_lab/new_file.py
@@ -1,6 +1,7 @@
 class IncorrectClass:
     def __init__(self):
-        print("Ми створили цей об'єкт.")
+        print("Ми створили цей об'єкту у файлі new_file.py.")
 
+print(f"{__name__} виконується.")
 if __name__ == "__main__":
     a = IncorrectClass()
